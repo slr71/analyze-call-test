186866bdfc502c42ea56d0d0475f40a97af19c9e
Sarah Roberts <sonoran.sarah@gmail.com>
2025-03-19 15:18:38 -0700

    fixed the clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.cyverse/analyze-call-test "0.0.1-SNAPSHOT"
   :description "Minimal test case"
-  :dependencies [[org.clojure/clojure "0.12.0"]]
-  :git-dependencies [["https://github.com/slr71/clojure-commons" "992e9bd1ba3a7c348038ad6f35d44cf916ae5093"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :git-dependencies [["https://github.com/slr71/clojure-commons" "main"]]
   :plugins [[lein-git-deps "0.0.2"]])
