ba49b07cdab99fbca13f351497d6f2c076cbd1bd
Sarah Roberts <sonoran.sarah@gmail.com>
2025-03-19 15:23:58 -0700

    added clj-kondo hooks and updated project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Minimal test case"
   :dependencies [[org.clojure/clojure "1.12.0"]]
   :git-dependencies [["https://github.com/slr71/clojure-commons" "main"]]
-  :plugins [[lein-git-deps "0.0.2"]])
+  :plugins [[com.github.clj-kondo/lein-clj-kondo "2025.02.20"]
+            [lein-git-deps "0.0.2"]])
